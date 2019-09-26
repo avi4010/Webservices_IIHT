2ff33c63f7a87aceef0f67f0f3d9526db64cee20
Avinash Seelam <avinash120196@gmail.com>
2019-09-26 19:48:18 +0530

    unwanted files are deleted

---
+++ src/test/java/com/studentapp/junit/FirstSerenityTest.java
@@ -0,0 +1,33 @@
+package com.studentapp.junit;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.runners.SerenityRunner;
+
+@RunWith(SerenityRunner.class)
+public class FirstSerenityTest {
+	
+	@BeforeClass
+	public static void init() {
+		
+		RestAssured.baseURI = "http://localhost:8080/student";
+		
+	}
+	
+	@Test
+	public void getAllStudents() {
+		
+		RestAssured.given()
+		.when()
+		.get("/list")
+		.then()
+		.log()
+		.all()
+		.statusCode(200);
+		
+	}
+
+}
